54c2914d0a2af5d8fcc1b097dc8d82ffcccd4937
Lucas Strand <lucas.strand@outlook.com>
2020-09-30 11:53:08 +0200

    player

--- Move.java
+++ Move.java
@@ -1,26 +1,26 @@
 import javax.swing.JButton;
 
 public class Move {
-  Player player;
+  // Player player;
   public Controller controller;
   static boolean gameover = false;
   
   public Move(){
     this.controller = controller;
-    this.player= new Player(this);
+    // this.player= new Player(this);
   }
   public void verifyMove(int _r,int _c){
     JButton button = controller.view.buttons[_r][_c];
     
     if(button.getText() != " " || gameover) return;
-        		if(player == 0) {
+        		if(Player.player == 0) {
                		button.setText("O");
                		/*if(checkifP1win()) {
                			controller.view.myLabel.setText("player 1 won!");
                			gameover = true;
-               			return;*/
-               		}
-        		 	player = 1;
+               			return;
+               		}*/
+        		 	Player.player = 1;
             		controller.view.myLabel.setText("player 2's turn");
         		}
         		else {
@@ -28,9 +28,9 @@ public class Move {
                		/*if(checkifP2win()) {
                			controller.view.myLabel.setText("player 2 won!");
                			gameover = true;
-               			return;*/
-               		}
-        			player = 0;
+               			return;
+               		}*/
+        			Player.player = 0;
             		controller.view.myLabel.setText("player 1's turn");
         		}
   }
--- Player.java
+++ Player.java
@@ -1,15 +1,16 @@
 public class Player {
-Move move;
- 
-public Player(){
+  Move move;
+  static public int player = 0;
 
-}
-public Player(Move move) {
-  this.move = new Move();
-}
-static private Player player;
+  public Player() {
 
-public void moveMade (int _r, int _c){
-  move.verifyMove(_r,_c);
-}
+  }
+
+  public Player(Move move) {
+    this.move = new Move();
+  }
+
+  public void moveMade(int _r, int _c) {
+    move.verifyMove(_r, _c);
+  }
 }
