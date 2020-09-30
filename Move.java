0751a155208e68f8e86b74b11266b9c404181b69
Lucas Strand <lucas.strand@outlook.com>
2020-09-30 15:23:06 +0200

    RuleEngine implemented

--- Move.java
+++ Move.java
@@ -3,10 +3,11 @@ import javax.swing.JButton;
 public class Move {
   public Controller controller;
 	static boolean gameover = false;
+	public RuleEngine ruleEngine;
 
   public Move(Controller _controller){
 		this.controller = _controller;
-
+		this.ruleEngine = new RuleEngine(_controller);
 		
 	}
 	
@@ -18,21 +19,21 @@ public class Move {
         		if(Player.player == 0) {
 									 button.setText("O");
 									//  System.out.println("test");
-               		/*if(checkifP1win()) {
+               		if(ruleEngine.checkifP1win(_r,_c)) {
                			controller.view.myLabel.setText("player 1 won!");
                			gameover = true;
                			return;
-               		}*/
+               		}
         		 	Player.player = 1;
             		controller.view.myLabel.setText("player 2's turn");
         		}
         		else {
                		button.setText("X");
-               		/*if(checkifP2win()) {
+               		if(ruleEngine.checkifP2win(_r,_c)) {
                			controller.view.myLabel.setText("player 2 won!");
                			gameover = true;
                			return;
-               		}*/
+               		}
         			Player.player = 0;
             		controller.view.myLabel.setText("player 1's turn");
         		}
--- RuleEngine.java
+++ RuleEngine.java
@@ -4,57 +4,58 @@ import javax.swing.JButton;
 
 public class RuleEngine {
   Move move;
-  Controller controller;
-  
-  public RuleEngine(Controller _controller){
-    this.move = new Move(controller);
-  }
-  public static boolean checkifP1win(int _r,int _c) {
-    JButton button= new JButton();
-    button = move.button[_r][_c];
+	Controller controller;
+	static JButton buttons[][];
+
+	public RuleEngine(Controller _controller) {
+		this.move = new Move(controller);
+	}
+
+	public static boolean checkifP1win(int _r, int _c) {
+		buttons[_r][_c] = new JButton();
     boolean win = false;
-		if(button[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
+		if(buttons[0][0].getText() == "O" && buttons[0][1].getText() == "O" && buttons[0][2].getText() == "O")
 			win = true;
-		if(board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
+		if(buttons[1][0].getText() == "O" && buttons[1][1].getText() == "O" && buttons[1][2].getText() == "O")
 			win = true;
-		if(board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
-			win = true;
-
-		if(board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
-			win = true;
-		if(board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
-			win = true;
-		if(board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
+		if(buttons[2][0].getText() == "O" && buttons[2][1].getText() == "O" && buttons[2][2].getText() == "O")
 			win = true;
 
-		if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+		if(buttons[0][0].getText() == "O" && buttons[1][0].getText() == "O" && buttons[2][0].getText() == "O")
 			win = true;
-		if(board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
+		if(buttons[0][1].getText() == "O" && buttons[1][1].getText() == "O" && buttons[2][1].getText() == "O")
+			win = true;
+		if(buttons[0][2].getText() == "O" && buttons[1][2].getText() == "O" && buttons[2][2].getText() == "O")
+			win = true;
+
+		if(buttons[0][0].getText() == "O" && buttons[1][1].getText() == "O" && buttons[2][2].getText() == "O")
+			win = true;
+		if(buttons[0][2].getText() == "O" && buttons[1][1].getText() == "O" && buttons[2][0].getText() == "O")
 			win = true;
 		
 		return win;
 	}
 
-	public static boolean checkifP2win() {
+	public static boolean checkifP2win(int _r, int _c) {
 		boolean win = false;
-		
-		if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
+		buttons[_r][_c] = new JButton();
+		if(buttons[0][0].getText() == "X" && buttons[0][1].getText() == "X" && buttons[0][2].getText() == "X")
 			win = true;
-		if(board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
+		if(buttons[1][0].getText() == "X" && buttons[1][1].getText() == "X" && buttons[1][2].getText() == "X")
 			win = true;
-		if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
-			win = true;
-
-		if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
-			win = true;
-		if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
-			win = true;
-		if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
+		if(buttons[2][0].getText() == "X" && buttons[2][1].getText() == "X" && buttons[2][2].getText() == "X")
 			win = true;
 
-		if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
+		if(buttons[0][0].getText() == "X" && buttons[1][0].getText() == "X" && buttons[2][0].getText() == "X")
 			win = true;
-		if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
+		if(buttons[0][1].getText() == "X" && buttons[1][1].getText() == "X" && buttons[2][1].getText() == "X")
+			win = true;
+		if(buttons[0][2].getText() == "X" && buttons[1][2].getText() == "X" && buttons[2][2].getText() == "X")
+			win = true;
+
+		if(buttons[0][0].getText() == "X" && buttons[1][1].getText() == "X" && buttons[2][2].getText() == "X")
+			win = true;
+		if(buttons[0][2].getText() == "X" && buttons[1][1].getText() == "X" && buttons[2][0].getText() == "X")
 			win = true;
 		
 		return win;	
