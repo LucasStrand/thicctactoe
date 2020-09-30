c8ce282a016e9528ef7c83254ab83cc2fcf5e399
Lucas Strand <lucas.strand@outlook.com>
2020-09-30 17:38:48 +0200

    true/false enable

--- Controller.java
+++ Controller.java
@@ -3,10 +3,13 @@ public class Controller {
   public View view;
   public View view2;
   public Player player;
-  
+  public boolean twoGui = true;
+
   public Controller(){
-    this.view= new View(this);
-    this.view2= new View(this);
+    this.view= new View(this); 
+    if(twoGui == true){
+      this.view2= new View(this);
+    }
     this.player=new Player(this);
   }
 
--- Move.java
+++ Move.java
@@ -1,48 +1,64 @@
 import javax.swing.JButton;
 
 public class Move {
-  public Controller controller;
+	public Controller controller;
 	static boolean gameover = false;
 	public RuleEngine ruleEngine;
 
-  public Move(Controller _controller){
+	public Move(Controller _controller) {
 		this.controller = _controller;
 		this.ruleEngine = new RuleEngine(controller);
-		
+
 	}
-	
-  public void verifyMove(int _r,int _c){
+
+	public void verifyMove(int _r,int _c){
 		JButton button = controller.view.buttons[_r][_c];
-		JButton buttonTwo = controller.view2.buttons[_r][_c];
-		// char moveChar [][] = 
+		JButton buttonTwo = new JButton();
+		if(controller.twoGui == true){
+			buttonTwo = controller.view2.buttons[_r][_c];
+		}
     
     if(button.getText() != " " || gameover) return;
         		if(Player.player == 0) {
 									 button.setText("O");
+									if(controller.twoGui == true)
+									{
 									 buttonTwo.setText("O");
+									}
 									//  System.out.println("test");
                		if(ruleEngine.checkifP1win()) {
 										 controller.view.myLabel.setText("player 1 won!");
+										 if(controller.twoGui == true)
+										 {
 										 controller.view2.myLabel.setText("player 1 won!");
+										}
                			gameover = true;
                			return;
                		}
         		 	Player.player = 1;
 								controller.view.myLabel.setText("player 2's turn");
+								if(controller.twoGui == true){
 								controller.view2.myLabel.setText("player 2's turn");
+								}
         		}
         		else {
 									 button.setText("X");
+									 if(controller.twoGui == true){
 									 buttonTwo.setText("X");
+									 }
                		if(ruleEngine.checkifP2win()) {
 										 controller.view.myLabel.setText("player 2 won!");
-										 controller.view2.myLabel.setText("player 2's won!");
+										 if(controller.twoGui == true){
+											 controller.view2.myLabel.setText("player 2's won!");
+										 }
                			gameover = true;
                			return;
                		}
         			Player.player = 0;
 								controller.view.myLabel.setText("player 1's turn");
-								controller.view2.myLabel.setText("player 2's turn");
-        		}
+								if(controller.twoGui == true){
+									controller.view2.myLabel.setText("player 2's turn");
+								}
+						}
   }
 }
